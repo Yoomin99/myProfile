1f3016f0b712d57a01baf604c3f3c1ac4519e501
Yoomin <ohmin9013@gmail.com>
2022-07-06 14:42:55 -0700

    update

--- src/code/python/2048/ai.py
+++ src/code/python/2048/ai.py
@@ -10,16 +10,17 @@ class Node:
     # Recommended: do not modify this __init__ function
     def __init__(self, state, player_type):
         self.state = (copy.deepcopy(state[0]), state[1])
-
         # to store a list of (direction, node) tuples
         self.children = []
-
         self.player_type = player_type
 
     # returns whether this is a terminal state (i.e., no children)
     def is_terminal(self):
-        #TODO: complete this
-        pass
+        if self.player_type == -1:
+            #print("is termianl: " , self.state[0])
+            return True
+
+        return False
 
 # AI agent. Determine the next move.
 class AI:
@@ -28,33 +29,129 @@ class AI:
         self.root = Node(root_state, MAX_PLAYER)
         self.search_depth = search_depth
         self.simulator = Game(*root_state)
+        
+    
+    
 
     # (Hint) Useful functions: 
     # self.simulator.current_state, self.simulator.set_state, self.simulator.move
 
+    # how to use function? 
+    # current_matrix, current_score = self.simulator.current_state()
+    # self.simulator.set_state
+    # self.simulator.set_state(current_matrix, current_score)
+#   self.state = (copy.deepcopy(state[0]), state[1])
+
+#         # to store a list of (direction, node) tuples
+#         self.children = []
+
+#         self.player_type = player_type
+#  root: player
+# - level 1: computer 
+# - level 2: player
+# - level 3: terminal with payoff 
+
+
+
     # TODO: build a game tree from the current node up to the given depth
     def build_tree(self, node = None, depth = 0):
-        print(self.simulator.current_state)
-        print(self.simulator.set_state)
-        print(self.simulator.move)
+
+        
+        if depth == 3:
+            for i in range(0,4):
+                
+                self.simulator.set_state(self.root.state[0], self.root.state[1])
+                if  self.simulator.move(i):
+                    newNode = Node(self.simulator.current_state(), CHANCE_PLAYER)
+                    self.root.children.append((i,newNode))
+
+            for i in range(len(self.root.children)):
+                (direction,newNode) = self.root.children[i]
+                self.build_tree(newNode, depth - 1)
+        
+        elif depth == 2:
+            self.simulator.set_state(node.state[0], node.state[1])
+            openTiles = self.simulator.get_open_tiles()
+            current_tile = self.simulator.tile_matrix
+            for i in openTiles:
+                (x,y) = i
+                self.simulator.tile_matrix = current_tile
+                if  self.simulator.tile_matrix[x][y] == 0:
+                    self.simulator.tile_matrix[x][y] = 2
+                    newNode = Node(self.simulator.current_state(), MAX_PLAYER)
+                    node.children.append((None,newNode))
+                    self.simulator.tile_matrix[x][y] = 0
+                
+
+            for i in node.children:
+                ( _ , newNode) = i
+                self.build_tree(newNode, depth - 1)
+        elif depth == 1:
+            for i in range(0,4):
+                self.simulator.set_state(node.state[0], node.state[1])
+                if self.simulator.move(i):
+                    newNode = Node(self.simulator.current_state(), -1)
+                    node.children.append((i,newNode))
+
+            
+
+                
 
 
-        pass
 
+        
+
+    
+        
     # TODO: expectimax calculation.
     # Return a (best direction, expectimax value) tuple if node is a MAX_PLAYER
     # Return a (None, expectimax value) tuple if node is a CHANCE_PLAYER
+
     def expectimax(self, node = None):
         # TODO: delete this random choice but make sure the return type of the function is the same
-        return random.randint(0, 3), 0
+        # print("EM root is : " , self.root.state[0])
+
+        if node.is_terminal():
+            (currState, currScore) = node.state
+            return  (None, currScore)
+
+        # max player
+        elif node.player_type == 0:
+            (currState, currScore) = self.simulator.current_state()
+
+            value = float('-inf')
+            new_direction = None
+    
+            for i in node.children: 
+                (temp_direction , temp_node) = i
+                
+                (D , score) = self.expectimax(temp_node)
+                temp_value = max(value , score)
+
+                
+
+                if value < temp_value or new_direction == None:
+                    new_direction = temp_direction
+                    value = temp_value
+
+            return (new_direction,value)
+        
+
+        #Chance player 
+        elif node.player_type == 1:
+            value = 0
+            chance = 1 / len(node.children) 
+
+            for i in node.children: 
+                (temp_direction , temp_node) = i
+                value = value + self.expectimax(temp_node)[1] * chance
+            return (None , value)
+
+   
 
     # Return decision at the root
     def compute_decision(self):
-        self.build_tree(self.root, self.search_depth)
+        self.build_tree(self.root, self.search_depth)     
         direction, _ = self.expectimax(self.root)
+        #print("Direction is " , direction )
         return direction
-
-    # TODO (optional): implement method for extra credits
-    def compute_decision_ec(self):
-        return random.randint(0, 3)
-
--- src/code/python/2048/main.py
+++ src/code/python/2048/main.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function
 import sys, time, math, random, os, argparse
 from game import Game
 from ai import AI
-from test import test, test_ec
+
 
 random.seed(0)
 
